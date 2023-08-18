5a824dfc23e9966e901618b0f42faee0cecd1e44
死鬼亨特 <y78978919@gmail.com>
2023-08-18 13:54:12 +0800

    加入專案檔案。

---
+++ ComeNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComeNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
